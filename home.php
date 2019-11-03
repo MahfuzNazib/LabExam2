5289c89d5cb97405025f22d29c88850eb0a3dd74
Nazib Mahfuz <41067328+MahfuzNazib@users.noreply.github.com>
2019-11-03 10:54:41 +0600

    Add files via upload
    html files

---
+++ home.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<title>Home</title>
+</head>
+
+<body>
+	<table border="1" width="100%">
+		<tr>
+			<td>
+				<table border="0">
+					<tr>
+						<td width="75%">
+							<h2>Company Name</h2>
+						</td>
+						<td>
+							<a href="home.php">Home</a>
+							<a href="login.php">Login</a>
+							<a href="registration.php">Registration</a>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		
+		<tr>
+			<td height="280px">
+				<h3>Welcome to xCompany</h3>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			<center>
+				Copyright@ 2017
+			</center>
+				
+			</td>
+		</tr>
+	</table>
+</body>
+
+</html>
---
+++ registration.php
@@ -0,0 +1,142 @@
+<html>
+<head>
+	<title>Home</title>
+</head>
+
+<body>
+	<table border="1" width="100%">
+		<tr>
+			<td>
+				<table border="1">
+					<tr>
+						<td width="75%">
+							<h2>Company Name</h2>
+						</td>
+						<td>
+							<a href="home.php">Home</a>
+							<a href="login.php">Login</a>
+							<a href="registration.php">Registration</a>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		
+		<tr>
+		<td>
+		<center>
+		<form action="#" method="POST">
+				<fieldset>
+			<legend>Registration</legend>
+			<table border="0">
+				<tr>
+					<td>Name: </td>
+					<td><input type="text" name=""  /></td>
+					
+				</tr>
+				<tr>
+					<td colspan="2">
+						<hr>
+					</td>
+				</tr>
+				
+				<tr>
+					<td>Email: </td>
+					<td><input type="mail" name=""  /></td>
+					
+				</tr>
+				<tr>
+					<td colspan="2">
+						<hr>
+					</td>
+				</tr>
+				
+				<tr>
+					<td>Username: </td>
+					<td><input type="text" name=""  /></td>
+					
+				</tr>
+				<tr>
+					<td colspan="2">
+						<hr>
+					</td>
+				</tr>
+				
+				<tr>
+					<td>Password:</td>
+					<td><input type="password" name="" /></td>
+				</tr>
+				<tr>
+				
+				<tr>
+					<td colspan="2">
+						<hr>
+					</td>
+				</tr>
+				<tr>
+				
+				<tr>
+					<td>Confirm Password:</td>
+					<td><input type="password" name="" /></td>
+				</tr>
+				<tr>
+				
+				<tr>
+					<td colspan="2">
+						<hr>
+					</td>
+				</tr>
+				
+				<tr>
+					<td>Gender</td>
+					<td>
+						<input type="radio" name="gender"/> Male
+						<input type="radio" name="gender"/> Female 
+					</td>
+				</tr>
+				
+				<tr>
+					<td colspan="2">
+						<hr>
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						DOB
+					</td>
+					<td><input type="date" name="" /></td>
+				</tr>
+				
+				<tr>
+					<td colspan="2">
+						<hr>
+					</td>
+				</tr>
+				
+				<tr>
+					<td></td>
+					<td>
+						<input type="reset" name="" value="reset">
+						<input type="submit" name="" value="submit">
+					</td>
+				</tr>
+			</table>
+		</fieldset>
+			</form>
+			</center>
+		</td>
+			
+		</tr>
+		<tr>
+			<td>
+			<center>
+				Copyright@ 2017
+			</center>
+				
+			</td>
+		</tr>
+	</table>
+</body>
+
+</html>
